1ffef0ebb1ea6644f97bdc3c314fd02197126976
Norberto Rodas <betens85@gmail.com>
2015-10-03 21:36:43 -0300

    Commit de prueba

---
+++ 2015taed2grupo02/src/org/ues21/taed2/principal/Principal.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.ues21.taed2.principal;
+
+/**
+ * Clase que representa el menu principal
+ * @author grupo02
+ *
+ */
+public class Principal {
+
+	public static void main(String[] args) {
+		
+	}
+}
