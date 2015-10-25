8bacf941af66ec4868eb10a6e23306ea98712c95
Norberto Rodas <betens85@gmail.com>
2015-10-24 21:27:24 -0300

    Refactoring de metodos arbol avl. FALTA JAVADOC

--- 2015taed2grupo02/src/org/ues21/taed2/estructura/ArbolAVL.java
+++ 2015taed2grupo02/src/org/ues21/taed2/estructura/ArbolAVL.java
@@ -24,64 +24,64 @@ public class ArbolAVL<T extends Comparable<T> & Codeable> extends ArbolBinarioDe
 		return TipoEstructura.AAVL;
 	}
 
-	private NodoArbol<T> insertar(NodoArbol<T> p, T datos, Boolean h) throws Exception {
-		NodoArbol<T> p1;
-		if (p == null) {
-			p = new NodoArbol<T>(datos);
+	private NodoArbol<T> insertar(NodoArbol<T> nodo, T datos, Boolean h) throws Exception {
+		NodoArbol<T> izq;
+		if (nodo == null) {
+			nodo = new NodoArbol<T>(datos);
 			h = Boolean.TRUE;
-		} else if (datos.compareTo(p.getDatos()) < 0) {
-			NodoArbol<T> iz;
-			iz = insertar(p.getIzquierdo(), datos, h);
-			p.setIzquierdo(iz);
+		} else if (datos.compareTo(nodo.getDatos()) < 0) {
+			NodoArbol<T> nodoIzq;
+			nodoIzq = insertar(nodo.getIzquierdo(), datos, h);
+			nodo.setIzquierdo(nodoIzq);
 			// vuelvo por los nodos del camino de búsqueda
 			if (h) {
 				// decrementa el factor de equilibrio por aumentar la altura de
 				// rama izquierda
-				switch (p.getFactorEquilibrio()) {
+				switch (nodo.getFactorEquilibrio()) {
 
 				case 1:
-					p.setFactorEquilibrio(0);
+					nodo.setFactorEquilibrio(0);
 					h = Boolean.FALSE;
 					break;
 				case 0:
-					p.setFactorEquilibrio(-1);
+					nodo.setFactorEquilibrio(-1);
 					break;
 				case -1:
 					// rotación izquierda
-					p1 = p.getIzquierdo();
-					if (p1.getFactorEquilibrio() == -1) {
-						p = rotacionIzqSimple(p, p1);
+					izq = nodo.getIzquierdo();
+					if (izq.getFactorEquilibrio() == -1) {
+						nodo = rotacionIzqSimple(nodo, izq);
 					} else {
-						p = rotacionIzqDoble(p, p1);
+						nodo = rotacionIzqDoble(nodo, izq);
 					}
 					h = Boolean.FALSE;
 				}
 			}
-		} else if (datos.compareTo(p.getDatos()) > 0) {
-			NodoArbol<T> dr;
-			dr = insertar(p.getDerecho(), datos, h);
-			p.setDerecho(dr);
+		} else if (datos.compareTo(nodo.getDatos()) > 0) {
+			NodoArbol<T> nodoDer;
+			nodoDer = insertar(nodo.getDerecho(), datos, h);
+			nodo.setDerecho(nodoDer);
 			// vuelta por los nodos del camino de búsqueda
 			if (h) {
 				// incrementa el factor de equilibrio por aumentar la altura de
 				// rama izquierda
-				switch (p.getFactorEquilibrio()) {
+				switch (nodo.getFactorEquilibrio()) {
 
 				case 1:
 					// rotación derecha
-					p1 = p.getDerecho();
-					if (p1.getFactorEquilibrio() == 1) {
-						p = rotacionDerSimple(p, p1);
+					izq = nodo.getDerecho();
+					if (izq.getFactorEquilibrio() == 1) {
+						nodo = rotacionDerSimple(nodo, izq);
 					} else {
-						p = rotacionDerDoble(p, p1);
+						nodo = rotacionDerDoble(nodo, izq);
 					}
 					h = Boolean.FALSE;
 					break;
 				case 0:
-					p.setFactorEquilibrio(1);
+					nodo.setFactorEquilibrio(1);
 					break;
 				case -1:
-					p.setFactorEquilibrio(0);
+					nodo.setFactorEquilibrio(0);
 					h = Boolean.FALSE;
 				}
 			}
@@ -89,111 +89,111 @@ public class ArbolAVL<T extends Comparable<T> & Codeable> extends ArbolBinarioDe
 			throw new Exception("No debe haber claves repetidas");
 		}
 
-		return p;
+		return nodo;
 	}
 
 	/**
 	 * rotacion Izquierda Simple o rotación izquierda izquierda
 	 * 
-	 * @param p
-	 * @param p1
+	 * @param nodo
+	 * @param nodoIzq
 	 * @return
 	 */
-	private NodoArbol<T> rotacionIzqSimple(NodoArbol<T> p, NodoArbol<T> p1) {
-		p.setIzquierdo(p1.getDerecho());
-		p1.setDerecho(p);
+	private NodoArbol<T> rotacionIzqSimple(NodoArbol<T> nodo, NodoArbol<T> nodoIzq) {
+		nodo.setIzquierdo(nodoIzq.getDerecho());
+		nodoIzq.setDerecho(nodo);
 		// se actualizan los factores de equilibrio
-		if (p1.getFactorEquilibrio() == -1) {
+		if (nodoIzq.getFactorEquilibrio() == -1) {
 			// inserción cumplida
-			p.setFactorEquilibrio(0);
-			p1.setFactorEquilibrio(0);
+			nodo.setFactorEquilibrio(0);
+			nodoIzq.setFactorEquilibrio(0);
 		} else {
-			p.setFactorEquilibrio(-1);
-			p1.setFactorEquilibrio(1);
+			nodo.setFactorEquilibrio(-1);
+			nodoIzq.setFactorEquilibrio(1);
 		}
-		return p1;
+		return nodoIzq;
 	}
 
 	/**
 	 * rotacion derecha simple o rotación derecha derecha
 	 *
-	 * @param p
-	 * @param p1
+	 * @param nodo
+	 * @param nodoDer
 	 * @return
 	 */
-	private NodoArbol<T> rotacionDerSimple(NodoArbol<T> p, NodoArbol<T> p1) {
-		p.setDerecho(p1.getIzquierdo());
-		p1.setIzquierdo(p);
+	private NodoArbol<T> rotacionDerSimple(NodoArbol<T> nodo, NodoArbol<T> nodoDer) {
+		nodo.setDerecho(nodoDer.getIzquierdo());
+		nodoDer.setIzquierdo(nodo);
 		// se actualizan los factores de equilibrio
-		if (p1.getFactorEquilibrio() == 1) {
+		if (nodoDer.getFactorEquilibrio() == 1) {
 			// inserción cumplida
-			p.setFactorEquilibrio(0);
-			p1.setFactorEquilibrio(0);
+			nodo.setFactorEquilibrio(0);
+			nodoDer.setFactorEquilibrio(0);
 		} else {
-			p.setFactorEquilibrio(1);
-			p1.setFactorEquilibrio(-1);
+			nodo.setFactorEquilibrio(1);
+			nodoDer.setFactorEquilibrio(-1);
 		}
-		return p1;
+		return nodoDer;
 	}
 
 	/**
 	 * rotación izquierda doble o rotación izquierda derecha
 	 * 
-	 * @param p
-	 * @param p1
+	 * @param nodo
+	 * @param nodo2
 	 * @return
 	 */
-	private NodoArbol<T> rotacionIzqDoble(NodoArbol<T> p, NodoArbol<T> p1) {
-		NodoArbol<T> p2;
+	private NodoArbol<T> rotacionIzqDoble(NodoArbol<T> nodo, NodoArbol<T> nodo2) {
+		NodoArbol<T> nodo2Der;
 
-		p2 = p1.getDerecho();
-		p.setIzquierdo(p2.getDerecho());
-		p2.setDerecho(p);
-		p1.setDerecho(p2.getIzquierdo());
-		p2.setIzquierdo(p1);
+		nodo2Der = nodo2.getDerecho();
+		nodo.setIzquierdo(nodo2Der.getDerecho());
+		nodo2Der.setDerecho(nodo);
+		nodo2.setDerecho(nodo2Der.getIzquierdo());
+		nodo2Der.setIzquierdo(nodo2);
 		// se actualizan los factores de equilibrio
-		if (p2.getFactorEquilibrio() == 1) {
-			p1.setFactorEquilibrio(-1);
+		if (nodo2Der.getFactorEquilibrio() == 1) {
+			nodo2.setFactorEquilibrio(-1);
 		} else {
-			p1.setFactorEquilibrio(0);
+			nodo2.setFactorEquilibrio(0);
 		}
-		if (p2.getFactorEquilibrio() == -1) {
-			p.setFactorEquilibrio(1);
+		if (nodo2Der.getFactorEquilibrio() == -1) {
+			nodo.setFactorEquilibrio(1);
 		} else {
-			p.setFactorEquilibrio(0);
+			nodo.setFactorEquilibrio(0);
 		}
-		p2.setFactorEquilibrio(0);
-		return p2;
+		nodo2Der.setFactorEquilibrio(0);
+		return nodo2Der;
 	}
 
 	/**
 	 * rotación derecha doble o rotación derecha izquierda
 	 * 
-	 * @param p
-	 * @param p1
+	 * @param nodo
+	 * @param nodo2
 	 * @return
 	 */
-	private NodoArbol<T> rotacionDerDoble(NodoArbol<T> p, NodoArbol<T> p1) {
-		NodoArbol<T> p2;
-		p2 = p1.getIzquierdo();
+	private NodoArbol<T> rotacionDerDoble(NodoArbol<T> nodo, NodoArbol<T> nodo2) {
+		NodoArbol<T> nodo2Izq;
+		nodo2Izq = nodo2.getIzquierdo();
 
-		p.setDerecho(p2.getIzquierdo());
-		p2.setIzquierdo(p);
-		p1.setIzquierdo(p2.getDerecho());
-		p2.setDerecho(p1);
+		nodo.setDerecho(nodo2Izq.getIzquierdo());
+		nodo2Izq.setIzquierdo(nodo);
+		nodo2.setIzquierdo(nodo2Izq.getDerecho());
+		nodo2Izq.setDerecho(nodo2);
 		// se actualizan los factores de equilibrio
-		if (p2.getFactorEquilibrio() == 1) {
-			p.setFactorEquilibrio(-1);
+		if (nodo2Izq.getFactorEquilibrio() == 1) {
+			nodo.setFactorEquilibrio(-1);
 		} else {
-			p.setFactorEquilibrio(0);
+			nodo.setFactorEquilibrio(0);
 		}
-		if (p2.getFactorEquilibrio() == -1) {
-			p1.setFactorEquilibrio(1);
+		if (nodo2Izq.getFactorEquilibrio() == -1) {
+			nodo2.setFactorEquilibrio(1);
 		} else {
-			p1.setFactorEquilibrio(0);
+			nodo2.setFactorEquilibrio(0);
 		}
-		p2.setFactorEquilibrio(0);
-		return p2;
+		nodo2Izq.setFactorEquilibrio(0);
+		return nodo2Izq;
 	}
 
 }
